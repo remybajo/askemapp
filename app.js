314a87b16e44a9b9dd62ff4ffac75ef6f1199c6d
remy <remy@MacBook-Air-de-remy.local>
2021-11-18 16:12:56 +0100

    heroku

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
